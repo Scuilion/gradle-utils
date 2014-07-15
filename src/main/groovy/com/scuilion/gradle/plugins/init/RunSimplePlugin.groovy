89ea6d732ef9fe71f00b39c40d7ba68f68325899
Kevin O'Neal <oneal.kevin@gmail.com>
2014-07-15 10:04:23 -0500

    don't check for applied plugins now

--- src/main/groovy/com/scuilion/gradle/plugins/init/RunSimplePlugin.groovy
+++ src/main/groovy/com/scuilion/gradle/plugins/init/RunSimplePlugin.groovy
@@ -3,8 +3,6 @@ package com.scuilion.gradle.plugins.init
 import org.gradle.api.Project
 import org.gradle.api.Plugin
 import org.gradle.api.tasks.JavaExec
-import org.gradle.api.plugins.BasePlugin
-import org.gradle.api.plugins.GroovyPlugin
 
 class RunSimplePlugin implements Plugin<Project> {
 
