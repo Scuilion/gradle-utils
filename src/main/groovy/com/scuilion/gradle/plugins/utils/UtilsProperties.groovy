0c7cec0bbde9c4110dc311d523f2770bdfd052ba
Kevin O'Neal <oneal.kevin@gmail.com>
2015-04-29 08:51:26 -0500

    changed group name

--- src/main/groovy/com/scuilion/gradle/plugins/utils/UtilsProperties.groovy
+++ src/main/groovy/com/scuilion/gradle/plugins/utils/UtilsProperties.groovy
@@ -2,5 +2,5 @@ package com.scuilion.gradle.plugins.utils
 
 class UtilsProperties {
     public static String DESCRIPTION = 'A utility class'
-    public static String GROUP = 'Utils'
+    public static String GROUP = 'Util'
 }
