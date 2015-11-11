0173b14919d0a8d763cb6e2a651e0a7219086ecb
Kevin O'Neal <oneal.kevin@gmail.com>
2015-11-10 19:43:26 -0600

    update gradle

--- src/main/groovy/com/scuilion/gradle/plugins/utils/UtilsPlugin.groovy
+++ src/main/groovy/com/scuilion/gradle/plugins/utils/UtilsPlugin.groovy
@@ -4,7 +4,6 @@ import org.gradle.api.Project
 import org.gradle.api.Plugin
 
 import com.scuilion.gradle.plugins.utils.init.RunSimple
-import com.scuilion.gradle.plugins.utils.vim.Syntastic
 import com.scuilion.gradle.plugins.utils.debug.Pather
 import com.scuilion.gradle.plugins.utils.debug.TestTiming
 
@@ -12,7 +11,6 @@ class UtilsPlugin implements Plugin<Project> {
 
     void apply(Project project) {
         RunSimple.addTask(project)
-        Syntastic.addTask(project)
         Pather.addTask(project)
         TestTiming.addTask(project)
     }
